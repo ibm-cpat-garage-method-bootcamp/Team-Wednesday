aabf825926eb8f4777216583daf7fd5e7906a8d4
Madeleine <madeleine.wilson@ibm.com>
2020-01-14 16:22:13 -0600

    core logic for wordwrap working

--- client/src/mfw-test/wordwrap.spec.js
+++ client/src/mfw-test/wordwrap.spec.js
@@ -1,5 +1,13 @@
 function wordwrap(myString, lineLength) {
-	let paragraph = myString;
+	let paragraph = "";
+	if (myString.length > lineLength) {
+		myString = myString.match(new RegExp('.{1,' + lineLength + '}', 'g'));
+	}
+	for (let i = 0; i < myString.length; i++) {
+		paragraph = paragraph.concat(myString[i]);
+		if (i < myString.length - 1)
+			paragraph = paragraph.concat('\n');
+	}
 
 	return paragraph;
 }
@@ -13,7 +21,19 @@ describe.only('wordwrap', () => {
 		expect(wordwrap('a', 1)).toEqual('a');
 	});
 
-	// test('string "aaaa bbbb cccc", 5 passed to wordwrap returns "aaaa \nbbbb \ncccc"', () => {
-	// 	expect(wordwrap('aaaa bbbb cccc', 5)).toEqual('aaaa \nbbbb \ncccc');
-	// });
+	test('arguments "aa", 1 are passed to wordwrap and returns paragraph "a\na"', () => {
+		expect(wordwrap('aa', 1)).toEqual('a\na');
+	});
+
+	test('arguments "a a", 2 are passed to wordwrap and returns paragraph "a \na"', () => {
+		expect(wordwrap('a a a', 4)).toEqual('a a \na');
+	});
+
+	test('string "aaaa bbbb cccc", 5 passed to wordwrap returns "aaaa \nbbbb \ncccc"', () => {
+		expect(wordwrap('aaaa bbbb cccc', 5)).toEqual('aaaa \nbbbb \ncccc');
+	});
+
+	test('string "aaaa bb cccc", 3 passed to wordwrap returns "aaa\na \nbb \nccc\nc"', () => {
+		expect(wordwrap('aaaa bb cccc', 3)).toEqual('aaa\na \nbb \nccc\nc');
+	});
 });
